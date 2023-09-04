2f31431573c9b3be912966e7da49e659620f9ee4
CentralFloridaAttorney <71934273+CentralFloridaAttorney@users.noreply.github.com>
2023-09-04 08:24:37 -0400

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./pages/Home";
+
+const App = () =>{
+    return (
+        <h1>
+            Welcome to React App that's build using Webpack and Babel separately
+            <Home/>
+        </h1>
+    )
+}
+
+export default App
